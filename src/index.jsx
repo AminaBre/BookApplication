55c40efed81a06f1c06a32d1dc1288a5c4967c84
Amina Brenneng <aminahb@live.no>
2021-03-21 12:07:34 +0100

    Application should run from server

---
+++ src/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter, Link} from "react-router-dom";
+import {Route, Switch} from "react-router";
+
+
+function Application() {
+    return <BrowserRouter>
+        <Switch>
+            <Route path={"/books"}>
+            <h1>List alle bøker</h1>
+        </Route>
+        <Route path={"/create"}>
+            <h1>Lag nye bøker</h1>
+        </Route>
+        <Route path={"/edit"}>
+            <h1>Rediger eksisterende bok</h1>
+        </Route>
+        <Route path={"/"}>
+            <h1>Bok-applikasjon hjemmeside</h1>
+            <ul>
+                <li><Link to={"/books"}>List alle bøker</Link></li>
+                <li><Link to={"/create"}>Lag nye bøker</Link></li>
+            </ul>
+        </Route>
+        <Route>
+            Siden du ser etter eksisterer ikke
+        </Route>
+    </Switch>
+    </BrowserRouter>;
+}
+
+ReactDOM.render(<Application />, document.getElementById("root"));
